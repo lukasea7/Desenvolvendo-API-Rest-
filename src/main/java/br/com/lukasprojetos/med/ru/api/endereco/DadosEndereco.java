27a5f79aec3749573d24365dcb2140708486d6d5
Lukas chaves <lucaschavesjr@gmail.com>
2025-05-28 15:54:07 +0100

    Criacao Controller , Conexao banco de dados

---
+++ src/main/java/br/com/lukasprojetos/med/ru/api/endereco/DadosEndereco.java
@@ -0,0 +1,7 @@
+package br.com.lukasprojetos.med.ru.api.endereco;
+
+public record DadosEndereco( String logadoro, String bairro, String cidade,
+                             String uf, String conplemento,
+                             String numero) {
+
+}
---
+++ src/test/java/br/com/lukasprojetos/med/ru/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lukasprojetos.med.ru.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

77ea0f47000379d6fd5e6c68459ceef44845af8c
Lukas chaves <lucaschavesjr@gmail.com>
2025-06-02 23:23:05 +0100

    Validação com Bean Validatin

--- src/main/java/br/com/lukasprojetos/med/ru/api/controller/MedicoControler.java
+++ src/main/java/br/com/lukasprojetos/med/ru/api/controller/MedicoControler.java
@@ -4,6 +4,7 @@ package br.com.lukasprojetos.med.ru.api.controller;
 import br.com.lukasprojetos.med.ru.api.medico.DadosCadastroMedico;
 import br.com.lukasprojetos.med.ru.api.medico.Medico;
 import br.com.lukasprojetos.med.ru.api.medico.MedicoReposritory;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ public class MedicoControler {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         reposritory.save(new Medico(dados));
 
 

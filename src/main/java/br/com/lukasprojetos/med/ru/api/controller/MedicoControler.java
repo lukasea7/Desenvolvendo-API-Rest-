a68b14d905092ee07bb01cf270a75b240fe0525e
Lukas chaves <lucaschavesjr@gmail.com>
2025-06-03 14:50:32 +0100

    Producão de dados API

--- src/main/java/br/com/lukasprojetos/med/ru/api/controller/MedicoControler.java
+++ src/main/java/br/com/lukasprojetos/med/ru/api/controller/MedicoControler.java
@@ -2,15 +2,16 @@ package br.com.lukasprojetos.med.ru.api.controller;
 
 
 import br.com.lukasprojetos.med.ru.api.medico.DadosCadastroMedico;
+import br.com.lukasprojetos.med.ru.api.medico.DadosListagemMedico;
 import br.com.lukasprojetos.med.ru.api.medico.Medico;
 import br.com.lukasprojetos.med.ru.api.medico.MedicoReposritory;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,12 +20,16 @@ public class MedicoControler {
     private MedicoReposritory reposritory;
 
 
-
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         reposritory.save(new Medico(dados));
 
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return reposritory.findAll().stream().map(DadosListagemMedico::new).toList();
 
     }
+
 }

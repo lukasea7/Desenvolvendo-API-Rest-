ed3611dc695b6b2c565b33235e0d53a593951c22
Lukas chaves <lucaschavesjr@gmail.com>
2025-06-27 15:27:48 +0100

    Criando Paginação

--- src/main/java/med/_con/api/controller/MedicoController.java
+++ src/main/java/med/_con/api/controller/MedicoController.java
@@ -3,13 +3,15 @@ package med._con.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med._con.api.medico.DadosCadastroMedico;
+import med._con.api.medico.DadosListagemMedico;
 import med._con.api.medico.Medico;
 import med._con.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -23,4 +25,11 @@ public class MedicoController {
         repository.save(new Medico(dados));
 
     }
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+
+
 }

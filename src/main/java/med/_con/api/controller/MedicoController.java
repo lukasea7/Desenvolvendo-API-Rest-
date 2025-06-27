427823f2abb23e2ec85b0cd39c84c39cc80cca8f
Lukas chaves <lucaschavesjr@gmail.com>
2025-06-27 14:23:26 +0100

    Criando Projeto Api Rest

---
+++ src/main/java/med/_con/api/controller/MedicoController.java
@@ -0,0 +1,26 @@
+package med._con.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med._con.api.medico.DadosCadastroMedico;
+import med._con.api.medico.Medico;
+import med._con.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+
+    }
+}
---
+++ src/main/java/med/_con/api/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package med._con.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}

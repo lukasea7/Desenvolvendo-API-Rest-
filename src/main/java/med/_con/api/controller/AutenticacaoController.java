a8b3db323c7c8750b796573e91439ac30f64d0e5
Lukas chaves <lucaschavesjr@gmail.com>
2025-07-08 22:37:57 +0100

    Criado Controller de autenticação

---
+++ src/main/java/med/_con/api/controller/AutenticacaoController.java
@@ -0,0 +1,32 @@
+package med._con.api.controller;
+
+import jakarta.validation.Valid;
+import med._con.api.domain.usuario.DadosAutenticacao;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Authenticator;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity efetuarLoging(@RequestBody @Valid DadosAutenticacao dados){
+
+      var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
+      var  authentication =  manager.authenticate(token);
+
+      return ResponseEntity.ok().build();
+
+    }
+}
---
+++ src/main/java/med/_con/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package med._con.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}

472550d9bdb70817d6857b13f301d0a7185c9b71
Lukas chaves <lucaschavesjr@gmail.com>
2025-07-11 16:10:48 +0100

    Implementado a Logica para recuperar Token

--- src/main/java/med/_con/api/infra/security/SecurityFilter.java
+++ src/main/java/med/_con/api/infra/security/SecurityFilter.java
@@ -1,4 +1,30 @@
 package med._con.api.infra.security;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Configuration
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+
+        filterChain.doFilter(request, response);
+
+
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
 }
